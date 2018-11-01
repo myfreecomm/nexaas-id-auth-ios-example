aee4609f9c379c88edb48937a5a511b09b6d3cab
randradedasilva <rodrigoandradesilva@icloud.com>
2018-11-01 14:45:02 -0300

    Created Params class

---
+++ nexaasId/Controller/Utils/Params.swift
@@ -0,0 +1,22 @@
+//
+//  Params.swift
+//  nexaasId
+//
+//  Created by Rodrigo Andrade on 29/10/18.
+//  Copyright © 2018 Rodrigo Andrade. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Params {
+    
+    static func credentials() -> [String: Any] {
+        return ["grant_type": Url.GRANT_TYPE_CREATE, "client_id": Url.CLIENT_ID, "client_secret": Url.CLIENT_SECRET]
+    }
+    
+    static func signIn(username: String, password: String) -> [String: Any] {
+        return ["username": username, "password": password, "grant_type": Url.GRANT_TYPE_CREDENTIALS, "client_id": Url.CLIENT_ID, "client_secret": Url.CLIENT_SECRET]
+    }
+    
+}
