d4ebdce055b1755b29719a9f9ec8155a3538a4a2
randradedasilva <rodrigoandradesilva@icloud.com>
2018-11-01 14:44:34 -0300

    Created NexaasIdUser and User model

---
+++ nexaasId/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  nexaasId
+//
+//  Created by Rodrigo Andrade on 30/10/18.
+//  Copyright © 2018 Rodrigo Andrade. All rights reserved.
+//
+
+import CodableAlamofire
+import Foundation
+
+struct User: Codable {
+    
+    let uuid: String
+    let full_name: String
+    let nickname: String
+    let email: String
+    let birth: Int
+    let gender: String?
+    let language: String?
+    let picture: String?
+    let timezone: String?
+    let country: String?
+    let city: String?
+    let bio: String?
+    
+}
